d5957d5aaf198d906cef74bfb42c920fb7ae69b2
Scott González <scott.gonzalez@gmail.com>
2016-07-29 13:46:21 -0400

    Don't require reloading the page to toggle implicit capture

--- background.js
+++ background.js
@@ -11,8 +11,18 @@ chrome.storage.local.get('disabled', ({ disabled }) => {
   updateBadge(disabled)
 
   chrome.browserAction.onClicked.addListener(() => {
+    // Store the disabled setting
     disabled = !disabled
-    updateBadge(disabled)
     chrome.storage.local.set({ disabled })
+
+    // Update the badge to show the new status
+    updateBadge(disabled)
+
+    // Update the existing pages to reflect the new status
+    chrome.tabs.query({}, (tabs) => {
+      tabs.forEach((tab) => {
+        chrome.tabs.sendMessage(tab.id, { disabled })
+      })
+    })
   })
 })
--- content.js
+++ content.js
@@ -1,8 +1,19 @@
-chrome.storage.local.get('disabled', ({ disabled }) => {
-  if (!disabled) {
-    window.addEventListener('pointerdown', (event) => {
-      if (event.target instanceof Element)
-        event.target.setPointerCapture(event.pointerId)
-    }, true)
+function handler (event) {
+  if (event.target instanceof Element) {
+    event.target.setPointerCapture(event.pointerId)
   }
-})
+}
+
+function toggleCapture ({ disabled }) {
+  if (disabled) {
+    window.removeEventListener('pointerdown', handler, true)
+  } else {
+    window.addEventListener('pointerdown', handler, true)
+  }
+}
+
+// Determine if the extension is disabled on load
+chrome.storage.local.get('disabled', toggleCapture)
+
+// Process changes to the disabled state
+chrome.runtime.onMessage.addListener(toggleCapture)
